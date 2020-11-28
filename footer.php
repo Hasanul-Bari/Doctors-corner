1547f9665836e22b54b5f9b1d4b4f24626bf5a1f
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-28 22:01:36 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+<footer class="t-bg2">
+  <div class="container">
+    <div class="row py-5">
+      <div class="col-md-4">
+        <img src="img/logo1.png" class="img-fluid my-4" alt="">
+        <p class="t-shad1">Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.<br><br><br></p>
+        <p class="t-shad1"> 
+          &copy; 2020-doctorscorner@gmail.com
+        </p>
+      </div>
+      <div class="col-md-2">
+        <h5 class="text-white mt-4">Links</h5>
+        <a href="#" class="t-shad1">Home</a><br>
+        <a href="#" class="t-shad1">Our Services</a><br>
+        <a href="#" class="t-shad1">Support</a><br>
+        <a href="#" class="t-shad1">Terms and Conditions</a>
+
+      </div>
+      <div class="col-md-2">
+        <h5 class="text-white mt-4">About Us</h5>
+        <a href="#" class="t-shad1">Sign In</a><br>
+        <a href="#" class="t-shad1">Register</a><br>
+        <a href="#" class="t-shad1">About Us</a><br>
+        <a href="#" class="t-shad1">Blog</a><br>
+        <a href="#" class="t-shad1">Contact Us</a><br>
+
+
+      </div>
+      <div class="col-md-4">
+        <h5 class="text-white mt-4">Contact Us</h5>
+        <p class="t-shad1">Get in touch with us via mail phone.We are waiting for<br><br> your call or message<br></p>
+        <button type="button" class="btn t-btn3 mt-3 px-5 text-white w-100">doctorscorner@gmail.com</button>
+        <a href="#"><i class="fab text-white fa-facebook primary fa-3x p-3"></i></a>
+        <a href="#"><i class="fab text-white fa-whatsapp primary fa-3x p-3"></i></a>
+        <a href="#"><i class="fab text-white fa-viber primary fa-3x p-3"></i></a>
+        <a href="#"><i class="fas text-white fa-mobile primary fa-3x p-3"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
