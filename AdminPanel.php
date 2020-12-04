09ad982024d766427bbe92eb6f340a2a59bb7930
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-12-04 18:53:18 +0600

    Create AdminPanel.php

---
+++ AdminPanel.php
@@ -0,0 +1,100 @@
+<?php 
+      require_once "pdo.php";
+      session_start();
+      
+      if(!isset($_SESSION['admin'])){
+        die("ACCESS DENIED");
+      }
+ ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!--    custom css-->
+  <link rel="stylesheet" href="custom/style_Admin.css">
+  <link rel="stylesheet" href="custom/responsive.css">
+
+  <!--Fontawsome css-->
+  <link rel="stylesheet" href="fontawesome/css/all.css" type="text/css">
+
+
+
+  <title>AdminPanel</title>
+</head>
+
+<body>
+
+  <!--  NavBar-->
+
+  <header class="t-nav sticky-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#"><img src="img/logo1.png" class="img-fluid" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent ">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="AdminPanel.php">Admin Panel</a>
+                </li>
+                          
+                <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="img/profile.png" class="img-fluid" alt="">&nbsp; Admin </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="logout.php">Logout</a>
+                            </div>
+                          </li>            
+              </ul>
+              
+              
+            </div>
+          </nav>
+        </div>
+
+      </div>
+    </div>
+  </header>
+
+  <!--  NavBar-->
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5 mt-5">
+          <a href="" type="button" class="btn btn-primary">Doctor List</a><br><br>
+          
+          <a href="AddDoctor.php"><button type="button" class="btn btn-success">Add Doctor</button></a><br><br>
+          <a href="AddDepartment.php"><button type="button" class="btn btn-success">Add Department</button></a>
+
+
+        </div>
+    
+        
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
