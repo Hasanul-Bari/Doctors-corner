8bb8324b989e4c4e7f59c9933960a2daf5d0537c
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-09-19 19:57:43 +0600

    Update PatientProfile.php

--- PatientProfile.php
+++ PatientProfile.php
@@ -1,8 +1,11 @@
 <?php
     require_once "pdo.php";
     session_start();
+
+    $patientID='';
     
     if(isset($_SESSION['patient'])){
+
       $stmt = $pdo->prepare("SELECT * FROM patients where Pid= :pid");
       $stmt->execute(array(
           ":pid" => $_SESSION['patient'])
@@ -21,7 +24,50 @@
   
       $age = $today->diff($bday);
       //echo $age->y;
+
+      $todaydate=$today->format('Ymd');
+      $todaydate=(int)$todaydate;
+
+      $patientID=$_SESSION['patient'];
             
+    }
+    else if(isset($_SESSION['doctor']) && isset($_GET['patient_id'])){
+
+      $stmt = $pdo->prepare("SELECT * FROM patients where Pid= :pid");
+      $stmt->execute(array(
+          ":pid" => $_GET['patient_id'])
+        );
+      $pt = $stmt->fetch(PDO::FETCH_ASSOC);
+      
+      $nm= htmlentities($pt['Name']);
+      $ph= htmlentities($pt['Phone']);
+      $bd= htmlentities($pt['DOB']);
+      $gd= htmlentities($pt['Gender']);
+      $wt= htmlentities($pt['Weight']);
+      $ht= htmlentities($pt['Height']);
+      
+      $today = new DateTime();
+      $bday = new DateTime($bd);
+  
+      $age = $today->diff($bday);
+      //echo $age->y;
+
+      $todaydate=$today->format('Ymd');
+      $todaydate=(int)$todaydate;
+
+      // getting the doctor name for navbar
+      $stmt = $pdo->prepare("SELECT Name FROM doctors where Did= :did");
+      $stmt->execute(array(
+          ":did" => $_SESSION['doctor'])
+        );
+      $dr = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+      $patientID=$_GET['patient_id'];
+
+    }
+    else if(isset($_SESSION['doctor'])){
+      die("Bad parameter");
     }
     else{
       die("ACCESS DENIED");
@@ -29,19 +75,36 @@
     
     
     
-    //fetching upcoming appointments
+    //fetching previous appointments
     
-    $sql="SELECT  consultations.Cid, doctors.Name, consultations.Cdate
+    $sql="SELECT  consultations.Cid, doctors.Name, consultations.Cdate, consultations.Ctime
           FROM consultations JOIN doctors
           ON consultations.Did=doctors.Did 
-          WHERE consultations.Pid= :pid
+          WHERE consultations.Pid= :pid and consultations.Cdate< :currentDate
           ORDER BY consultations.Cdate";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array(
-            ":pid" => $_SESSION["patient"])
+            ":pid" => $patientID,
+            ":currentDate" => $todaydate)
           );
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $previous = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    //fetching upcoming appointments
+    
+    $sql="SELECT  consultations.Cid, doctors.Name, consultations.Cdate, consultations.Ctime
+          FROM consultations JOIN doctors
+          ON consultations.Did=doctors.Did 
+          WHERE consultations.Pid= :pid and consultations.Cdate>= :currentDate
+          ORDER BY consultations.Cdate";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+            ":pid" => $patientID,
+            ":currentDate" => $todaydate)
+          );
+    $upcomings = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     
 
@@ -101,21 +164,34 @@
                 <li class="nav-item">
                   <a class="nav-link" href="#">FAQ</a>
                 </li>
+
+                <?php 
+                      if(isset($_SESSION['patient'])){
+                        ?>
+                            <li class="nav-item dropdown active">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="img/profile.png" class="img-fluid" alt="">&nbsp; <?= $nm ?> </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="PatientProfile.php">Profilee</a>
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                            </div></li>
+                        <?php  
+                      }
+                      else if(isset($_SESSION['doctor'])){
+                        ?>
+                            <li class="nav-item dropdown active">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="img/profile.png" class="img-fluid" alt="">&nbsp; <?=htmlentities($dr['Name'])?> </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="doctorProfile.php">Profile</a>
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                            </div></li>
+                        <?php  
+                      }
+                      
                 
-              
-                      
-                <li class="nav-item dropdown active">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="img/profile.png" class="img-fluid" alt="">&nbsp; <?= $nm ?> </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="PatientProfile.php">Profile</a>
-                      <a class="dropdown-item" href="logout.php">Logout</a>
-                    </div>
-                </li>
-                      
-                      
-                
-                 
+                 ?>
+
                 
               </ul>
               
@@ -134,18 +210,72 @@
 
   <section>
     <div class="container">
+
+     <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 text-center">
+          <h3>Patient's Details</h3>
+          <?php 
+            if ( isset($_SESSION['success']) ) {
+                echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
+                unset($_SESSION['success']);
+            }
+          ?>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+
+      <div class="row  my-3">
+          <div class="col-md-3"></div>
+          <div class="col-md-6 border border-dark rounded py-3">
+              
+              <h4>Patient's Name : <?= $nm ?> </h4>
+              <h5>Age : <?= $age->y.' years '.$age->m.' months '.$age->d.' days' ?></h5>
+              <h5>Sex : <?= $gd ?></h5>
+              <h5>Height : <?= $ht.' cm' ?> </h5>
+              <h5>Weight : <?= $wt.' kg' ?> </h5>
+              <h5>Contact : <?= $ph ?></h5>
+              <h5>Date of Birth : <?= $bd ?></h5>
+
+
+
+              <?php 
+                if(isset($_SESSION['patient'])){
+              ?>
+                  <a type="button" class="btn btn-primary" href="PatientProfileEdit.php">Edit Profile</a>
+              <?php 
+                }
+              ?>
+
+
+
+          </div>
+          <div class="col-md-3"></div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3"></div>
+          <div class="col-md-6 text-center">
+            <h3>Consultation List</h3>
+          </div>
+        <div class="col-md-3"></div>
+      </div>
+
       <div class="row mt-3">
+
         <div class="col-md-5">
           <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10 text-center">
-              <h3>My Consultation List</h3>
+              
+              <h4>Previous Consultations</h4>
+              
             </div>
             <div class="col-md-1"></div>
           </div>
-          
+         
           <?php 
-                foreach ( $rows as $row ){
+                foreach ( $previous as $row ){
                       
                       $consultDate=htmlentities($row['Cdate']);
                       $consultDate = substr_replace($consultDate, '-', 4 , 0);
@@ -153,13 +283,14 @@
                       $cdate=new DateTime($consultDate);
                       
                   
-                    ?>
+          ?>
                           <div class="row border border-dark rounded my-3">
                             <div class="col-md-1"></div>
                             <div class="col-md-10 py-3">
                               
-                              <h4><?=htmlentities($row['Name'])?></h4>
+                              <h5>Consultant: <?=htmlentities($row['Name'])?></h5>
                               <h5>On <?= $cdate->format('l, jS  F Y') ?></h5>
+                              <h5>At <?= htmlentities($row['Ctime']) ?></h5>
                               
                               <button type="button" class="btn btn-primary">Details</button>
                               <button type="button" class="btn btn-success">Status</button>
@@ -171,43 +302,53 @@
                 }
           
            ?>
-          
-          
         </div>
+
+
         <div class="col-md-2"></div>
+
+
+
         <div class="col-md-5">
           <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10 text-center">
-              <h3>Details</h3>
-              <?php 
-                  if ( isset($_SESSION['success']) ) {
-                      echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
-                      unset($_SESSION['success']);
-                  }
-               ?>
+              <h4>Upcoming Consultations</h4>
             </div>
             <div class="col-md-1"></div>
           </div>
-          <div class="row border border-dark rounded my-3">
-            <div class="col-md-1"></div>
-            <div class="col-md-10 py-3">
-              
-              <h4>Patient's Name : <?= $nm ?> </h4>
-              <h5>Age : <?= $age->y.' years '.$age->m.' months '.$age->d.' days' ?></h5>
-              <h5>Sex : <?= $gd ?></h5>
-              <h5>Height : <?= $ht.' cm' ?> </h5>
-              <h5>Weight : <?= $wt.' kg' ?> </h5>
-              <h5>Contact : <?= $ph ?></h5>
-              <h5>Date of Birth : <?= $bd ?></h5>
-
-              <a type="button" class="btn btn-primary" href="PatientProfileEdit.php">Edit Profile</a>
-
-
-            </div>
-            <div class="col-md-1"></div>
-          </div>
+          <?php 
+                foreach ( $upcomings as $row ){
+                      
+                      $consultDate=htmlentities($row['Cdate']);
+                      $consultDate = substr_replace($consultDate, '-', 4 , 0);
+                      $consultDate = substr_replace($consultDate, '-', 7 , 0);
+                      $cdate=new DateTime($consultDate);
+                      
+                  
+          ?>
+                          <div class="row border border-dark rounded my-3">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-10 py-3">
+                              
+                              <h5>Consultant: <?=htmlentities($row['Name'])?></h5>
+                              <h5>On <?= $cdate->format('l, jS  F Y') ?></h5>
+                              <h5>At <?= htmlentities($row['Ctime']) ?></h5>
+                              
+                              
+                              <a type="button" class="btn btn-primary" href="prescription.php?cid=<?= $row['Cid']?>">Details</a>
+                              <button type="button" class="btn btn-success">Status</button>
+                            
+                            </div>
+                            <div class="col-md-1"></div>
+                          </div>                    
+                    <?php 
+                }
+          
+           ?>
         </div>
+
+
       </div>
     </div>
   </section>
