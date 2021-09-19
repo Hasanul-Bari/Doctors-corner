434e38a6afaa451adbc13e2d0d743609cf6269e3
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-09-19 19:57:04 +0600

    Update doctorProfile.php

--- doctorProfile.php
+++ doctorProfile.php
@@ -154,7 +154,20 @@
   <section>
     <div class="container">
 
+      <?php 
+          if ( isset($_SESSION['error']) ) {
+            echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+            unset($_SESSION['error']);
+          }
+          if ( isset($_SESSION['success']) ) {
+              echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
+              unset($_SESSION['success']);
+          }
+      ?>
+
       <div class="row my-3">
+
+      
         <div class="col-md-1"></div>
         <div class="col-md-4">
           <img src="img/doctorIcon.jpg" class="img-fluid" alt="">
